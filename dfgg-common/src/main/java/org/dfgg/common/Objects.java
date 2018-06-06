3647fb3bef9b6f8009f9cc2f11abd8b21754ea47
dfggking <835317619@qq.com>
2018-06-06 18:12:41 +0800

    a

---
+++ dfgg-common/src/main/java/org/dfgg/common/Objects.java
@@ -0,0 +1,21 @@
+package org.dfgg.common;
+
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author jinyingfei - 835317619@qq.com
+ * @create 2018-06-06
+ */
+public final class Objects {
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+}
